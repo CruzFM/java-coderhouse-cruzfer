c5ff46bf48070f1893c0b6b340b73e09bd28818c
Fer Cruz <cruzfernandomartin94@gmail.com>
2023-04-20 20:57:51 -0300

    First task finished. Ready to hand in.

--- firstTask/src/main/java/classOne.java
+++ firstTask/src/main/java/classOne.java
@@ -1,2 +1,17 @@
 public class classOne {
+    public static void main(String[] args) {
+
+        //We declare a variable that will contain a float number.
+        float mainNumber = 3.9405830f;
+
+        //We declare a variable that will cast our mainNumber into an integer.
+        int intPart = (int) mainNumber;
+
+        //We use operators to obtain the floating part out of our mainNumber.
+        float floatPart = mainNumber - intPart;
+
+        //We print the results as requested in the task.
+        System.out.println("The integer part of our main number is: " + intPart);
+        System.out.println("The float part of our main number is: " + floatPart);
+    }
 }
