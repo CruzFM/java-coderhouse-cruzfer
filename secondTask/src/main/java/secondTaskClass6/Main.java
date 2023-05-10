69ad360a335aea45b7392601040a66badf1bc6ff
Fer Cruz <cruzfernandomartin94@gmail.com>
2023-05-09 22:38:19 -0300

    second task started. Classes created.

---
+++ secondTask/src/main/java/secondTaskClass6/Main.java
@@ -0,0 +1,13 @@
+package secondTaskClass6;
+/*
+CONSIGNA
+Agregar 5 objetos del tipo People a una lista.
+Crear la logica para ordenar alfabeticamente la lista y mostrar en consola de acuerdo al siguiente criterio>
+ordenado por nombre
+ordenado por apellido
+ordenado inversamente por apellido.
+*/
+
+
+public class Main {
+}
---
+++ secondTask/src/main/java/secondTaskClass6/Person.java
@@ -0,0 +1,45 @@
+package secondTaskClass6;
+
+public class Person {
+    private String name;
+
+    private String lastname;
+
+    private final String[] names = {"Fernando", "Lautaro", "Lucas", "Gianfranco", "Luciana"};
+
+    private final String[] lastnames = {"Sacco", "Cruz", "Rondon", "Kim", "Cipriano"};
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+
+    public Person() {
+        this.name = ;
+    }
+
+    private String getRandomItem (String[] list){
+        int randomIndex = (int) Math.round(Math.random() * list.length - 1);
+        return list[randomIndex];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+
+        if (!name.equals(person.name)) return false;
+        return lastname.equals(person.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + lastname.hashCode();
+        return result;
+    }
+}
